62ad0015f560c47365caf9ed7cc99e90cf6ae428
Skyfire27 <yann.rinjard@edu.ece.fr>
2018-04-17 18:20:43 +0200

    Yann
    Création panneau, bouton et text

--- PROJET POO Java/Java/src/vue/Fenetre.java
+++ PROJET POO Java/Java/src/vue/Fenetre.java
@@ -18,16 +18,30 @@ import java.io.*;
 public class Fenetre extends JFrame
 {
     private JPanel pan;//fenêtre de visualisation de l'interface d'origine
+    private JLabel lab;//instancie la texte
+    // instancie les boutons
+    private JButton b1;
+    private JButton b2;
+    private JButton b3;
     
     public Fenetre(){
         setTitle("Hopital d'Albert et Yann");
         setSize(600,500);
         pan=new JPanel();//instancier le panneau
+        lab=new JLabel("projet");
+        b1=new JButton("test");
+        b2=new JButton("test");
+        b3=new JButton("test");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//quitte le programme lorsque l'utilisateur ferme la fenêtre
     }
     
     public void affiche(){
         pan.setLayout(new FlowLayout());//mise en forme de bouton à la suite
+        pan.add(lab);
+        pan.add(b1);
+        pan.add(b2);
+        pan.add(b3);
+        add(pan);
         this.setVisible(true);
     }
 }
