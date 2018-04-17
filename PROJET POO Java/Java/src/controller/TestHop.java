c37497b4ffb6e5436ebd0e741949691521568f82
PC-400TAN\Albert <tanalbert.02119@gmail.com>
2018-04-17 16:41:40 +0200

    Alb
    Regrouper les deux codes

--- PROJET POO Java/Java/src/controller/TestHop.java
+++ PROJET POO Java/Java/src/controller/TestHop.java
@@ -13,14 +13,19 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+<<<<<<< HEAD
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
+=======
+import vue.Fenetre;
+>>>>>>> Interface.Java
 
 /**
  *
  * @author Albert
  */
 public class TestHop {
+<<<<<<< HEAD
    
 
     public static void main(String args[]) {
@@ -33,5 +38,17 @@ public class TestHop {
                 Logger.getLogger(TestHop.class.getName()).log(Level.SEVERE, null, ex);
             }
             
+=======
+    private static Fenetre fen=new Fenetre();
+    public static void main(String args[]){
+        try {
+            Connexion local = new Connexion("projet","root","");
+        } catch (SQLException ex) {
+            Logger.getLogger(TestHop.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(TestHop.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        fen.affiche();
+>>>>>>> Interface.Java
     }
 }
