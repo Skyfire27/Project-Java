6a7ced82526ea5e99fb5f341da1cc9cfb8856118
PC-400TAN\Albert <tanalbert.02119@gmail.com>
2018-04-18 17:44:16 +0200

    Alb
    Optimisation de la saisie

--- PROJET POO Java/Java/src/controller/TestHop.java
+++ PROJET POO Java/Java/src/controller/TestHop.java
@@ -5,16 +5,6 @@
  */
 package controller;
 
-import java.sql.Connection;
-import java.sql.DatabaseMetaData;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import modele.Recherche;
 
 import vue.Fenetre;
@@ -28,26 +18,8 @@ public class TestHop {
     private static Fenetre fen = new Fenetre();
 
     public static void main(String args[]) {
-        try {
-            Connexion local = new Connexion("projet", "root", "");
-            ArrayList<String> liste = local.remplirChampsTable("malade");
-            ArrayList<String> listebis = local.remplirChampsRequete("select * from malade");
-            Iterator<String> it = liste.iterator();
-            Iterator<String> its = listebis.iterator();
-            while (it.hasNext()) {
-                System.out.println(it.next());
-            }
-            while (its.hasNext()) {
-                System.out.println(its.next());
-            }
-            Recherche rech = new Recherche();
-            rech.saisie();            
-
-        } catch (SQLException ex) {
-            Logger.getLogger(TestHop.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (ClassNotFoundException ex) {
-            Logger.getLogger(TestHop.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        Recherche rech = new Recherche();
+        rech.saisie();
         fen.affiche();
     }
 }
