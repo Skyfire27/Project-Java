7e919aea85ecbe920f9d05511ce8f01d9106070d
PC-400TAN\Albert <tanalbert.02119@gmail.com>
2018-04-18 16:08:08 +0200

    Alb
    Debut de la classe Recherche

--- PROJET POO Java/Java/src/modele/Recherche.java
+++ PROJET POO Java/Java/src/modele/Recherche.java
@@ -6,6 +6,7 @@
 package modele;
 import controller.Connexion;
 import java.sql.SQLException;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -14,6 +15,37 @@ import java.util.logging.Logger;
  * @author Albert
  */
 public class Recherche {
-   
+    private Connexion local;
+    private boolean valider;
+    private String saisie;
+    private int nombre;
+    
+    public Recherche(){
+        try {
+            local =new Connexion("projet", "root", "");
+            valider = false;
+            saisie = "";
+            nombre = 0;
+                    } catch (SQLException | ClassNotFoundException ex) {
+            Logger.getLogger(Recherche.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public Recherche(String database, String username, String mdp){
+        try {
+            local =new Connexion(database, username, mdp);
+            valider = false;
+            saisie = "";
+            nombre = 0;
+                    } catch (SQLException | ClassNotFoundException ex) {
+            Logger.getLogger(Recherche.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void Saisie(String demande){
+        Scanner sc= new Scanner(System.in);
+        
+        
+    }
     
 }
