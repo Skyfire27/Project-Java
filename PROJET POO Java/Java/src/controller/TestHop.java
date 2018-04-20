76a38b18530707fcc3f09f55da6aaef53da24596
Skyfire27 <yann.rinjard@edu.ece.fr>
2018-04-20 17:14:27 +0200

    Yann

--- PROJET POO Java/Java/src/controller/TestHop.java
+++ PROJET POO Java/Java/src/controller/TestHop.java
@@ -7,10 +7,6 @@ package controller;
 
 import modele.Recherche;
 import vue.Fenetre;
-import org.jfree.chart.ChartFactory;
-import org.jfree.chart.ChartFrame;
-import org.jfree.chart.JFreeChart;
-import org.jfree.data.general.DefaultPieDataset;
 
 /**
  *
@@ -21,16 +17,7 @@ public class TestHop {
     private static Fenetre fen = new Fenetre(rech);
 
     public static void main(String args[]) {
-        DefaultPieDataset dataset =new DefaultPieDataset();
-        dataset.setValue("Category 1", 43.2);
-        dataset.setValue("Category 2", 27.9);
-        dataset.setValue("Category 3", 79.5);
         
-        JFreeChart chart;
-        chart = ChartFactory.createPieChart("Sample Pie Chart", dataset, true, true, false);
-        ChartFrame frame = new ChartFrame("First", chart);
-        frame.pack();
         fen.affiche();
-        frame.setVisible(true);
     }
 }
