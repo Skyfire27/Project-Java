61606e4ba51f2f084df63639408e9c9980678c1b
PC-400TAN\Albert <tanalbert.02119@gmail.com>
2018-04-21 17:59:05 +0200

    Mise a jour
    Reste a fixer la condition, mais probleme

--- PROJET POO Java/Java/src/modele/Update.java
+++ PROJET POO Java/Java/src/modele/Update.java
@@ -9,6 +9,7 @@ import controller.Connexion;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -42,14 +43,40 @@ public class Update {
     }
     
     public void modifier(ArrayList<String> donnee){
-        try {
-            
-            
-            local.executeUpdate(requeteMaj);
-        } catch (SQLException ex) {
-            Logger.getLogger(Update.class.getName()).log(Level.SEVERE, null, ex);
+        if (null != donnee.get(0))switch (donnee.get(0)) {
+            case "chambre":
+                requeteMaj = "UPDATE "+donnee.get(0)+" SET code_service = '"+donnee.get(1)+"', no_chambre = '"+donnee.get(2)+"', surveillant = '"+donnee.get(3)+"' WHERE condition";
+                break;
+            case "docteur":
+                requeteMaj = "UPDATE "+donnee.get(0)+" SET numero = '"+donnee.get(1)+"', specialite = '"+donnee.get(2)+"' WHERE condition";
+                break;
+            case "employe":
+                requeteMaj = "UPDATE "+donnee.get(0)+" SET numero = '"+donnee.get(1)+"', nom = '"+donnee.get(2)+"', prenom = '"+donnee.get(3)+"', adresse = '"+donnee.get(4)+"', tel = '"+donnee.get(5)+"' WHERE condition";
+                break;
+            case "hospitalisation":
+                requeteMaj = "UPDATE "+donnee.get(0)+" SET no_malade = '"+donnee.get(1)+"', code_service = '"+donnee.get(2)+"', no_chambre = '"+donnee.get(3)+"', lit = '"+donnee.get(4)+"' WHERE condition";
+                break;
+            case "infirmier":
+                requeteMaj = "UPDATE "+donnee.get(0)+" SET numero = '"+donnee.get(1)+"', code_service = '"+donnee.get(2)+"', rotation = '"+donnee.get(3)+"', salaire = '"+donnee.get(4)+"' WHERE condition";
+                break;
+            case "malade":
+                requeteMaj = "UPDATE "+donnee.get(0)+" SET numero = '"+donnee.get(1)+"', nom = '"+donnee.get(2)+"', prenom = '"+donnee.get(3)+"', adresse = '"+donnee.get(4)+"', tel = '"+donnee.get(5)+"', mutuelle = '"+donnee.get(6)+"' WHERE condition";
+                break;
+            case "service":
+                requeteMaj = "UPDATE "+donnee.get(0)+" SET code = '"+donnee.get(1)+"', nom = '"+donnee.get(2)+"', batiment = '"+donnee.get(3)+"', directeur = '"+donnee.get(4)+"' WHERE condition";
+                break;
+            case "soigne":
+                requeteMaj = "UPDATE "+donnee.get(0)+" SET no_docteur = '"+donnee.get(1)+"', no_malade = '"+donnee.get(2)+"' WHERE condition";
+                break;
+            default:
+                break;
         }
-        
+            
+        for (int i=0; i<donnee.size();i++)
+        {
+            donnee.get(i);
+        }
+        requeteMaj = "UPDATE table SET colonne_1 = 'valeur 1', colonne_2 = 'valeur 2', colonne_3 = 'valeur 3' WHERE condition";
        
     }
 }
